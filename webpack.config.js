1031bc207dc7360e4ced57afdb0e933db6765b86
Thomas <itsthomas@outlook.com>
2018-08-05 20:53:56 +0100

    Done

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
             },
           },
           { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
           { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
