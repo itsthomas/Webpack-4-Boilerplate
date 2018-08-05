62fc9022f827d43a4739e993c53e6a64966189a3
Thomas <itsthomas@outlook.com>
2018-08-05 21:43:09 +0100

    FontAwesome integration

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import { addFn, nameFn } from './vendor';
-// import '../index.html';
 import '../styles/styles.scss';
+// Font-Awesome 5.2
+require('../../node_modules/@fortawesome/fontawesome-free/css/all.css');
 
 addFn(10, 12);
 nameFn('Richard');
