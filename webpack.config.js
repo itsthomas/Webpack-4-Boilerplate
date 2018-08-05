9d60b0a2294307cfe2aa17735b6b787f90cc47e4
Thomas <itsthomas@outlook.com>
2018-08-05 17:35:55 +0100

    Can load only background images

--- webpack.config.js
+++ webpack.config.js
@@ -8,36 +8,46 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    // Serve this folder
+    contentBase: path.join(__dirname, 'dist'),
+    // overlay: true,
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       // Sass and CSS
       {
-        test: /\.(sa|sc|c)ss$/,
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // Image loader
+      {
+        test: /\.(gif|png|jpe?g)$/i,
         use: [
-          { loader: 'style-loader' },
-
-          // Generate a CSS file in Memory
-          // { loader: 'css-loader', options: { url: true, sourceMap: true } },
-
-          // Generate inline CSS
-          { loader: 'css-loader' },
-
-          { loader: 'sass-loader', options: { sourceMap: true } },
+          { loader: 'file-loader', options: { name: '[name].[hash][ext]', outputPath: 'images/' } },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
         ],
       },
-      // {
-      //   test: /\.s?css/,
-      //   use: [
-      //     'style-loader',
-      //     'css-loader',
-      //     'sass-loader'
-      //   ]
-      // }
     ],
   },
-  devServer: {
-    // Serve this folder
-    contentBase: path.join(__dirname, 'dist'),
-  },
-  devtool: 'source-map',
 };
